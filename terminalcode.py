541e5b052a32f776606ccea3534e88ab0aef7e62
NataliekohNP <93971361+NataliekohNP@users.noreply.github.com>
2023-01-31 15:50:31 +0800

    Delete terminalcode.py

--- terminalcode.py
+++
@@ -1,4 +0,0 @@
-import os
-# process1 = os.system("py -m pytest --edge --html=report_edge.html --maximize -n=4 --screenshot ")
-process2 = os.system("py -m pytest --safari --html=report_safari.html --screenshot ")
-# process3 = os.system("py -m pytest --firefox --html=report_firefox.html --maximize -n=4 --screenshot ")
